d43eddda601f4e54cad6343cd9abb83eb104f966
Will Cork <Will@sirwilliam19.local>
2013-04-06 16:44:12 -0400

    helper methods to get injured, uninjured peasants

--- src/ProbabilisticSearchAgent.java
+++ src/ProbabilisticSearchAgent.java
@@ -1,7 +1,9 @@
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.agent.Agent;
@@ -14,6 +16,7 @@ public class ProbabilisticSearchAgent extends Agent {
 	//Approximation of the chances a random square has a tower
 	private Double pTower = .00616;
 	private Double[][] towerProbs;
+	private Map<UnitView, Integer> peasants;
 
 	public ProbabilisticSearchAgent(int playernum) {
 		super(playernum);
@@ -22,6 +25,14 @@ public class ProbabilisticSearchAgent extends Agent {
 	@Override
 	public Map<Integer, Action> initialStep(StateView state, HistoryView history) {
 		towerProbs = new Double[state.getXExtent()][state.getYExtent()];
+		peasants = new HashMap<UnitView, Integer>();
+
+		for (UnitView unit : state.getAllUnits()) {
+			if (unit.getTemplateView().getName().equalsIgnoreCase("Peasant")) {
+				peasants.put(unit, unit.getHP());
+			}
+		}
+
 		return null;
 	}
 
@@ -35,9 +46,43 @@ public class ProbabilisticSearchAgent extends Agent {
 	public Map<Integer, Action> middleStep(StateView state, HistoryView history) {
 		Map<Integer, Action> toReturn = new HashMap<Integer, Action>();
 		
+		Set<UnitView> injured = checkInjuries(state);
+		Set<UnitView> uninjured = getUninjured(state);
 		
+		for (UnitView peasant : injured) {
+			
+		}
 		
 		return toReturn;
+	}
+	
+	private Set<UnitView> checkInjuries(StateView state) {
+		Set<UnitView> injured = new HashSet<UnitView>();
+		
+		for (UnitView unit : state.getAllUnits()) {
+			if (unit.getTemplateView().getName().equalsIgnoreCase("Peasant")) {
+				if (unit.getHP() < peasants.get(unit.getHP())) {
+					injured.add(unit);
+					peasants.put(unit, unit.getHP());
+				}
+			}
+		}
+		
+		return injured;
+	}
+	
+	private Set<UnitView> getUninjured(StateView state) {
+		Set<UnitView> uninjured = new HashSet<UnitView>();
+		
+		for (UnitView unit : state.getAllUnits()) {
+			if (unit.getTemplateView().getName().equalsIgnoreCase("Peasant")) {
+				if (unit.getHP() >= peasants.get(unit.getHP())) {
+					uninjured.add(unit);
+				}
+			}
+		}
+		
+		return uninjured;
 	}
 
 	@Override
