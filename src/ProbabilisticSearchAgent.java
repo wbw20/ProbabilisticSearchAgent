9e1af953edf3c29c1a2716e67ec501aca1dd6f95
William B. Wettersten <wbw20@case.edu>
2013-04-06 15:41:56 -0400

    initialization step

--- src/ProbabilisticSearchAgent.java
+++ src/ProbabilisticSearchAgent.java
@@ -1,22 +1,27 @@
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.agent.Agent;
 import edu.cwru.sepia.environment.model.history.History.HistoryView;
 import edu.cwru.sepia.environment.model.state.State.StateView;
+import edu.cwru.sepia.environment.model.state.Unit.UnitView;
 
 
 public class ProbabilisticSearchAgent extends Agent {
+	//Approximation of the chances a random square has a tower
+	private Double pTower = .00616;
+	private Double[][] towerProbs;
 
 	public ProbabilisticSearchAgent(int playernum) {
 		super(playernum);
 	}
 
 	@Override
-	public Map<Integer, Action> initialStep(StateView arg0, HistoryView arg1) {
-		// TODO Auto-generated method stub
+	public Map<Integer, Action> initialStep(StateView state, HistoryView history) {
+		towerProbs = new Double[state.getXExtent()][state.getYExtent()];
 		return null;
 	}
 
@@ -27,9 +32,12 @@ public class ProbabilisticSearchAgent extends Agent {
 	}
 
 	@Override
-	public Map<Integer, Action> middleStep(StateView arg0, HistoryView arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<Integer, Action> middleStep(StateView state, HistoryView history) {
+		Map<Integer, Action> toReturn = new HashMap<Integer, Action>();
+		
+		
+		
+		return toReturn;
 	}
 
 	@Override
