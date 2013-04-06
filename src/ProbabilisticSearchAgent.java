efc1b238811bd866389c5a317aef38e179348273
Will Cork <Will@sirwilliam19.local>
2013-04-06 17:32:28 -0400

    agent runs, peasants die

--- src/ProbabilisticSearchAgent.java
+++ src/ProbabilisticSearchAgent.java
@@ -15,8 +15,9 @@ import edu.cwru.sepia.environment.model.state.Unit.UnitView;
 public class ProbabilisticSearchAgent extends Agent {
 	//Approximation of the chances a random square has a tower
 	private Double pTower = .00616;
+	private Double pShotGivenTower = .75;
 	private Double[][] towerProbs;
-	private Map<UnitView, Integer> peasants;
+	private Map<Integer, Integer> peasants;
 
 	public ProbabilisticSearchAgent(int playernum) {
 		super(playernum);
@@ -25,11 +26,17 @@ public class ProbabilisticSearchAgent extends Agent {
 	@Override
 	public Map<Integer, Action> initialStep(StateView state, HistoryView history) {
 		towerProbs = new Double[state.getXExtent()][state.getYExtent()];
-		peasants = new HashMap<UnitView, Integer>();
+		peasants = new HashMap<Integer, Integer>();
 
 		for (UnitView unit : state.getAllUnits()) {
 			if (unit.getTemplateView().getName().equalsIgnoreCase("Peasant")) {
-				peasants.put(unit, unit.getHP());
+				peasants.put(unit.getID(), unit.getHP());
+			}
+		}
+		
+		for (int x = 0; x < towerProbs.length; x++) {
+			for (int y = 0; y < towerProbs[x].length; y++) {
+				towerProbs[x][y] = pTower;
 			}
 		}
 
@@ -50,10 +57,72 @@ public class ProbabilisticSearchAgent extends Agent {
 		Set<UnitView> uninjured = getUninjured(state);
 		
 		for (UnitView peasant : injured) {
-			
+			for (int x = peasant.getXPosition() - 5; x < peasant.getXPosition() + 5; x++) {
+				if (x < state.getXExtent() && x > 0) {
+					for (int y = peasant.getYPosition() - 5; y < peasant.getYPosition() + 5; y++) {
+						if (y < state.getYExtent() && y > 0) {
+							towerProbs[x][y] *= (pShotGivenTower*pTower)/getPShot(peasant, state);
+						}
+					}
+				}
+			}
 		}
 		
+		for (UnitView peasant : uninjured) {
+			for (int x = peasant.getXPosition() - 5; x < peasant.getXPosition() + 5; x++) {
+				if (x < state.getXExtent() && x > 0) {
+					for (int y = peasant.getYPosition() - 5; y < peasant.getYPosition() + 5; y++) {
+						if (y < state.getYExtent() && y > 0) {
+							towerProbs[x][y] *= ((1 - pShotGivenTower)*pTower)/(1 - getPShot(peasant, state));
+						}
+					}
+				}
+			}
+		}
+		
+		for (UnitView peasant : state.getAllUnits()) {
+			if (peasant.getTemplateView().getName().equalsIgnoreCase("Peasant")) {
+				int xTile = 0;
+				int yTile = 0;
+				Double probability = Double.MAX_VALUE;
+				
+				for (int x = peasant.getXPosition() - 5; x < peasant.getXPosition() + 5; x++) {
+					if (x < state.getXExtent() && x > 0) {
+						for (int y = peasant.getYPosition() - 5; y < peasant.getYPosition() + 5; y++) {
+							if (y < state.getYExtent() && y > 0) {
+								if (x > peasant.getXPosition() || y < peasant.getYPosition()) {
+									if (probability > towerProbs[x][y]) {
+										xTile = x;
+										yTile = y;
+										probability = towerProbs[x][y];
+									}
+								}
+							}
+						}
+					}
+				}
+				
+				toReturn.put(peasant.getID(), Action.createCompoundMove(peasant.getID(), xTile, yTile));
+			}
+		}			
+		
 		return toReturn;
+	}
+	
+	private Double getPShot(UnitView peasant, StateView state) {
+		Double total = 0.0;
+		
+		for (int x = peasant.getXPosition() - 5; x < peasant.getXPosition() + 5; x++) {
+			if (x < state.getXExtent() && x > 0) {
+				for (int y = peasant.getYPosition() - 5; y < peasant.getYPosition() + 5; y++) {
+					if (y < state.getYExtent() && y > 0) {
+						total += towerProbs[x][y];
+					}
+				}
+			}
+		}
+		
+		return 1 - Math.pow(1 - pShotGivenTower, total);
 	}
 	
 	private Set<UnitView> checkInjuries(StateView state) {
@@ -61,9 +130,9 @@ public class ProbabilisticSearchAgent extends Agent {
 		
 		for (UnitView unit : state.getAllUnits()) {
 			if (unit.getTemplateView().getName().equalsIgnoreCase("Peasant")) {
-				if (unit.getHP() < peasants.get(unit.getHP())) {
+				if (unit.getHP() < peasants.get(unit.getID())) {
 					injured.add(unit);
-					peasants.put(unit, unit.getHP());
+					peasants.put(unit.getID(), unit.getHP());
 				}
 			}
 		}
@@ -76,7 +145,7 @@ public class ProbabilisticSearchAgent extends Agent {
 		
 		for (UnitView unit : state.getAllUnits()) {
 			if (unit.getTemplateView().getName().equalsIgnoreCase("Peasant")) {
-				if (unit.getHP() >= peasants.get(unit.getHP())) {
+				if (unit.getHP() >= peasants.get(unit.getID())) {
 					uninjured.add(unit);
 				}
 			}
